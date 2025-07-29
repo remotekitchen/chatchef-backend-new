2224ec453f465e210beda555f3a359a9e463d6c6
Sohag Hossain <gdsohag360@gmail.com>
2025-07-29 11:32:14 +0600

    update project name

--- hungrytiger/settings/defaults.py
+++ hungrytiger/settings/defaults.py
@@ -81,7 +81,7 @@ INSTALLED_APPS = [
 
 ]
 
-SERVICE_ACCOUNT_KEY_PATH = os.path.join(BASE_DIR, 'chatchef', 'settings', 'serviceAccountKey.json')
+SERVICE_ACCOUNT_KEY_PATH = os.path.join(BASE_DIR, 'hungrytiger', 'settings', 'serviceAccountKey.json')
 cred = credentials.Certificate(SERVICE_ACCOUNT_KEY_PATH)
 try:
     FIREBASE_APP = get_app() 
@@ -108,7 +108,7 @@ MIDDLEWARE = [
     # 'allauth.account.middleware.AccountMiddleware',
 ]
 
-ROOT_URLCONF = "chatchef.urls"
+ROOT_URLCONF = "hungrytiger.urls"
 
 TEMPLATES = [
     {
@@ -127,7 +127,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "chatchef.wsgi.application"
+WSGI_APPLICATION = "hungrytiger.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -399,7 +399,7 @@ AWS_QUERYSTRING_AUTH = False
 mapbox_api_key = env.str("MAPBOX_KEY")
 
 
-ASGI_APPLICATION = 'chatchef.asgi.application'
+ASGI_APPLICATION = 'hungrytiger.asgi.application'
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
